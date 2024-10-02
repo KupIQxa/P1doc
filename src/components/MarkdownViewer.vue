5ee568030641e7a6401237241d6c0b146617363b
Кирилл <kikin-2015@mail.ru>
2024-10-02 17:10:30 +0300

    Готовый тестовый проект с поддержкой MD

---
+++ src/components/MarkdownViewer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-html="compiledMarkdown" class="markdown"></div>
+</template>
+
+<script>
+import { marked } from 'marked'; // Импортируйте правильно
+
+
+export default {
+  props: {
+    markdown: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    compiledMarkdown() {
+      return marked(this.markdown);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.markdown {
+  /* Здесь можно добавить стили для улучшения внешнего вида Markdown */
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import MarkdownViewer from '../components/MarkdownViewer.vue';
+import gettingStarted from '../docs/getting-started.md'; // Импортируйте ваш Markdown файл
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'GettingStarted',
+      component: {
+        render(h) {
+          return h(MarkdownViewer, {
+            props: {
+              markdown: gettingStarted
+            }
+          });
+        }
+      }
+    },
+    // Вы можете добавить другие маршруты для других файлов документации
+  ]
+});
